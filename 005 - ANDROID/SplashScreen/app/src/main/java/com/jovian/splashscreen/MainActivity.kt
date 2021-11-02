4f29cb99e3ad4a812a608acc9b4c2266ada0780d
Jorge Victoria Andreu <JorgeVictoria@users.noreply.github.com>
2021-11-02 18:29:08 +0100

    update 02nov21 1830

---
+++ 005 - ANDROID/SplashScreen/app/src/main/java/com/jovian/splashscreen/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.jovian.splashscreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setTheme(R.style.Theme_SplashScreen)
+
+        setContentView(R.layout.activity_main)
+    }
+}
